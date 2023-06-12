a80026494b8ec8161eb76ac454550e87088abada
kimKyeongbeom <bibix321@naver.com>
2023-06-12 12:00:51 +0900

    notFound 오류 해결 중 - 1

--- todolist/app/not-found.tsx
+++ todolist/app/not-found.tsx
@@ -1,8 +1,11 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function NotFound() {
   const router = useRouter();
-  router.push("/");
+  useEffect(() => {
+    router.push("/");
+  }, []);
   return <></>;
 }
